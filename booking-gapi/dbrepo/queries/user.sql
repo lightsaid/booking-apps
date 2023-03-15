440dddd341467f8c32efcd3b35aa5db51f85990f
lightsaid <lightsaid@163.com>
2023-03-15 17:35:22 +0800

    feat: 搭建booking-gapi项目，编写proto

---
+++ booking-gapi/dbrepo/queries/user.sql
@@ -0,0 +1,19 @@
+-- name: CreateUser :one
+INSERT INTO tb_users (
+    phone_number,
+    name
+) VALUES($1, $2) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM tb_users WHERE id = $1 AND deleted_at IS NULL LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE tb_users 
+SET 
+    name = $2,
+    avatar = $3,
+    openid = $4,
+    unionid = $5,
+    updated_at = $6
+WHERE id = $1 AND deleted_at IS NULL
+RETURNING *;
---
+++ booking-gapi/server/user_server.go
@@ -0,0 +1,5 @@
+package server
+
+type UserStore interface {
+	CreateUser()
+}
